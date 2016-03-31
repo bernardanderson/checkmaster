7d77f0ae94e6e1c466cd60dd8e87e7de84ae3441
Bernard Anderson <bernard.anderson@gmail.com>
2016-03-30 23:09:58 -0500

    Added dynamic div insertions and color I've added dynamic div insertions based on "category" and I also figured out that to minimize the "bouncing" of elements when changing the display of something from invisible to visible you need to use visibility to hidden.

--- js/domElements.js
+++ js/domElements.js
@@ -2,26 +2,44 @@
 
 var Checkserver = (() => {
 
+  var draggableDivType = { 
+    payor: [
+    '<div id="payor-address" class="title-container">',
+    '<p class="info-font">Payor Address</p>',
+    '<div id="payor-setting" class="button-container remove-view">'
+    ],
+
+    drawee: [
+    '<div id="drawee-address" class="title-container">',
+    '<p class="info-font">Drawee Address</p>',
+    '<div id="drawee-setting" class="button-container remove-view">'
+    ]
+
+  }
+
   return {
 
-    insertElement: (sentElement) => { 
-      $("body").prepend(sentElement);
+    insertElement: (sentElementBody) => $("body").prepend(sentElementBody),
+
+    makeDragDivElement: (sentCategory) => {
+
+      let dragInputDiv = 
+        `<div class="draggable">
+        ${Checkserver.getDraggableDivType()[sentCategory][0]}
+        ${Checkserver.getDraggableDivType()[sentCategory][1]}
+        <img class="settings-icon" src="img/settings.png" alt="">
+        </div>
+        <textarea class="inputter"></textarea>
+        ${Checkserver.getDraggableDivType()[sentCategory][2]}
+        <p class="button-row font">Font</p>
+        <p class="button-row font-size">Font Size</p>
+        <p class="button-row justification">Justification</p>
+        </div></div>`
+
+      return dragInputDiv;
     },
 
-    makeDragDiv: () => {
-      let dragInputDiv = `<div id="" class="draggable dragging">
-      <div class="title-container">
-      <p class="info-font">User Address</p>
-      <img id="settings" class="settings-icon" src="img/settings.png" alt="">
-      </div>
-      <textarea id="user-address" class="inputter"></textarea>
-      <div id="btn-container" class="button-container remove-view">
-      <p class="button-row">Font</p>
-      <p class="button-row">Font Size</p>
-      </div></div>`
-      return dragInputDiv;
-    }
-
+    getDraggableDivType: () => draggableDivType,
 
   };
 
--- js/myJqueryCalls.js
+++ js/myJqueryCalls.js
@@ -1,14 +1,17 @@
 
 // Immediately Invoked JQuery Events
 $(() => { 
-          // Sets the click event for the settings button to toggle the input box font settings
-          $(".draggable").draggable(); 
-          
-          // Sets the click event for the settings button to toggle the input box font settings
-          $("#settings").click( () => { $("#btn-container").toggleClass("remove-view") });
-        }
-);
+  // Makes items with the "draggable" class draggable
+  $(".draggable").draggable(); 
+  
+  // Sets the click event for the settings button to toggle the input box font settings
+  $(".draggable").click( () => { 
+    if (event.target.className.includes("settings-icon")) {
+      $(event.currentTarget).children(".button-container").toggleClass("remove-view");
+      $(event.currentTarget).children(".inputter").toggleClass("select-object");
+    };
+  });
+});
 
-
-Checkserver.insertElement(Checkserver.makeDragDiv());
-Checkserver.insertElement(Checkserver.makeDragDiv());
+Checkserver.insertElement(Checkserver.makeDragDivElement('payor'));
+Checkserver.insertElement(Checkserver.makeDragDivElement('drawee'));
